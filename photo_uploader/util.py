9921720c56b3d59358b40602c2b8813a8682a734
Edward Bridges <github@eqbridges.com>
2018-05-06 21:06:47 -0400

    add utilities for getting md5, bytes from a file, and dealing with types

--- photo_uploader/util.py
+++ photo_uploader/util.py
@@ -1,5 +1,15 @@
-from logging import basicConfig, INFO, DEBUG
 from os import path
+from logging import basicConfig, INFO, DEBUG
+from hashlib import md5
+from functools import partial
+from pathlib import Path
+from magic import Magic
+
+
+SUPPORTED_EXTENSIONS = (
+    '.jpg', '.jpeg', '.png', '.mov', '.mp4'
+)
+
 
 def configure_logging(level):
   if not level:
@@ -17,3 +27,37 @@ def item_folder(item):
     return None
   folder = path.dirname(item)
   return folder[2:] if(folder.startswith('./')) else folder
+
+
+def valid_type(item):
+  ext = Path(item.lower()).suffix
+  return ext in SUPPORTED_EXTENSIONS
+
+
+def md5sum(filename, block_size=2**20):
+  '''
+  Returns MD5 checksum for given file.
+  '''
+  with open(filename, mode='rb') as file:
+    d = md5()
+    for buf in iter(partial(file.read, block_size), b''):
+      d.update(buf)
+  return d.hexdigest()
+
+
+def mime_type(filename):
+  '''
+  Returns the mime type of the given file.
+  '''
+  mime = Magic(mime=True)
+  return mime.from_file(filename)
+
+
+def read_item_data(filename):
+  '''
+  Returns the bytes of the given file as a buffer.
+  '''
+  with open(filename, "rb") as binary_file:
+      # Reads the whole file at once
+      return binary_file.read()
+
